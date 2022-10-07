c8f43550b5ffab656993cf53851c9c33cc220215
leumani jospin <jospinunifer@gmail.com>
2022-10-07 13:31:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-row">
+      <InputWrapper value="High" numberOfQuestionSelected="0"  totalQuestions="14" @click="changeBorderColor" :class="[isActive ? 'border-[#EF5DA8]' : 'border-[#DADBDD]']"/>
+    </div>
+</template>
+
+<script setup>
+import InputWrapper from './components/InputWrapper.vue';
+import { ref } from 'vue'
+const isActive = ref(false)   
+function changeBorderColor(){
+  isActive.value = true
+} 
+
+</script>
---
+++ src/components/InputWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-row w-[256.3px] h-[38px] bg-white border-[0.47px] border-solid border-[#919FAF] rounded-lg p-[10px] bg-white mx-auto mt-8 relative">
+        <input type="radio" id="value" name="difficulty" :value="value" class="accent-[#EF5DA8]">
+            <div class="not-italic font-light text-sm leading-[18px] text-[#919FAF] w-[50px] h-[18px] font-['Outfit']">
+                <label for="value" class="pl-2">{{ value }}</label>
+               <label for="value" class="pl-[155px]">{{ numberOfQuestionSelected }}/{{ totalQuestions}}</label>
+            </div>
+   </div>
+</template>
+
+<script setup>
+defineProps({
+    value: {
+        type: String,
+        required: true
+    },
+    numberOfQuestionSelected: {
+        type: Number,
+        required: true
+    },
+    totalQuestions: {
+        type: Number,
+        required: true
+    }
+    
+  })
+  
+</script>
